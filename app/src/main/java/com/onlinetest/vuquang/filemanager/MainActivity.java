69d4e82f7afc629a804b331a5a559d51011687db
Nguyễn Vũ Quang <vuquangkhtn@gmail.com>
2018-06-09 13:30:47 +0700

    [All] init project

---
+++ app/src/main/java/com/onlinetest/vuquang/filemanager/MainActivity.java
@@ -0,0 +1,25 @@
+package com.onlinetest.vuquang.filemanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.onlinetest.vuquang.filemanager.utils.FLog;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+//            try {
+//                BasicFileAttributes fileAttributes = Files.readAttributes(Paths.get("abc"),BasicFileAttributes.class);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+        FLog.show("test");
+    }
+}
